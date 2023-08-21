8d8d90dafaed5bc10858e8a030d15efda746a465
James Yu Cao <caoyu19920414@gmail.com>
2023-08-21 09:40:08 -0400

    updated landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import "./LandingPage.css";
 
 function LandingPage() {
   const sessionUser = useSelector(state => state.session.user);
@@ -10,9 +11,20 @@ function LandingPage() {
       < Redirect to="/my-home" />
     </>)
   }
-  return (<>
-    <h1>Landing Page</h1>
-  </>)
+  return (<div className="landing-page-container">
+    <div className="landing-page-intro">
+      <h1>Flow</h1>
+      <h2>SoundCloud Clone <span>Music</span> App</h2>
+      <p>with React, Flask, SQLAlchemy, AWS S3</p>
+    </div>
+    <div className="landing-page-features">
+      <h2>Upload your favorite songs</h2>
+      <h2>Listen and comments</h2>
+    </div>
+
+
+
+  </div>)
 }
 
 export default LandingPage
